eea486454f4854a0a5af2fd8e3eb478793d29d5b
agix <florian.gaultier@gmail.com>
2015-04-23 14:30:06 +0200

    Fix filename printing

--- static/js/fileUpload.js
+++ static/js/fileUpload.js
@@ -1,27 +1,22 @@
-$(function(){  
-    var info = {  
+$(function(){
+    var info = {
       action: '../fileUpload/',
-      name: 'uploadfile', 
+      name: 'uploadfile',
       onSubmit: function(file, ext){ // On submit we do nothing, it'd be nice to do something but mheh..
       //console.log('Starting...');
-      },  
+      },
       onComplete: function(file, response){
         // Require the editor..
         var padeditor = require('ep_etherpad-lite/static/js/pad_editor').padeditor;
-        
-        // result i.e. "/up/c79133b2c8a97533cc397f8d325ce17a.jpg", trimmed whitespace
-        var filePath = response.replace(/^\s+|\s+$/g, '');
-        // "http://example.com/subfolder/p/padID" -> "http://example.com/subfolder"
-        var etherpadRoot = document.location.href.replace(/\/p\/[^\/]+$/,'');
-        // "http://example.com/subfolder/up/c79133b2c8a97533cc397f8d325ce17a.jpg"
-        var fileUri = etherpadRoot + filePath;
-        
+
+        var fileUri = response.replace(/^\s+|\s+$/g, '');
+
         // Puts the actual URL in the pad..
         padeditor.ace.replaceRange(undefined, undefined, " " + fileUri + " ");
         // Put the caret back into the pad
         padeditor.ace.focus();
-      }  
+      }
     };
 
-    new AjaxUpload($('#uploadFileSubmit'), info);  
+    new AjaxUpload($('#uploadFileSubmit'), info);
 });
